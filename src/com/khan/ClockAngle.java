435ccc6b28063d0ede05ba28630a7d86ff19b357
shadabbahadara <shadabbahadara@gmail.com>
2020-12-09 12:12:02 +0530

    angle between MH and HH of a clock

---
+++ src/com/khan/ClockAngle.java
@@ -0,0 +1,22 @@
+package com.khan;
+
+public class ClockAngle {
+
+	public static String calculateAngleTrick(int hour, int minute) {
+		double angle = Math.abs(30 * hour - 5.5 * minute);
+		return Math.min(360 - angle, angle) + " degree";
+	}
+
+	public static String calculateAngleConcept(int hour, int minute) {
+		double initialAngle = 30 * hour;
+		double hourAngle = 0.5 * minute;
+		double minuteAngle = 6 * minute;
+		double angle = Math.abs(minuteAngle - (initialAngle + hourAngle));
+		return Math.min(360 - angle, angle) + " degree";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(calculateAngleTrick(12, 0));
+		System.out.println(calculateAngleConcept(12, 0));
+	}
+}
